1d40cabe599df8d9c7cab15e62feb5422677766c
yungcxn <can.nayci@gmx.de>
2024-06-14 20:45:00 +0200

    Added some files

---
+++ home/.config/hypr/customkill.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Dispatch command to kill the active window
+hyprctl dispatch killactive
+sleep 0.01
+
+# Capture the current workspace
+current_workspace=$(hyprctl activeworkspace)
+
+# Extract the workspace ID
+output="${current_workspace#*ID }"
+n="${output:0:1}"
+
+# Loop while there are no windows and the workspace ID is not "1"
+while [ ! -z "$(hyprctl activeworkspace | grep 'windows: 5')" ] && [ "${n}" != "1" ]; do
+    hyprctl dispatch workspace m-1
+    sleep 0.01
+    current_workspace=$(hyprctl activeworkspace)
+    # Extract the workspace ID
+    output="${current_workspace#*ID }"
+    n="${output:0:1}"
+done
